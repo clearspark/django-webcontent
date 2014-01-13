2972a3a6ec38d03260a4d859e560220b2bacfe01
Matthys Kroon <matthysk@gmail.com>
2014-01-13 06:54:17 +0200

    Add list_display fields for FileUpload admin

--- admin.py
+++ admin.py
@@ -1,23 +1,11 @@
 #Content app
 from django.contrib import admin
-from csdjango.WebContent.models import Page, FileUpload, ContentTag
-try:
-    from tinymce.widgets import TinyMCE
-    from tinymce.widgets import AdminTinyMCE
-    tinyMCE = True
-except ImportError:
-    tinyMCE = False
-    print "Could not import TinyMCE"
+from csdjango.WebContent.models import Page, FileUpload, ContentTag, Hyperlink
 
-from django.db import models
-
-class PageAdmin(admin.ModelAdmin):
-
-    if tinyMCE:
-        formfield_overrides = {
-            models.TextField: {'widget': TinyMCE(attrs={'cols': 200, 'rows': 50})},
-            }
+class FileUploadAdmin(ContentAdmin):
+    list_display = ["slug", "title", "get_static_url"]
 
 admin.site.register(Page,PageAdmin)
-admin.site.register(FileUpload)
+admin.site.register(FileUpload, FileUploadAdmin)
 admin.site.register(ContentTag)
+admin.site.register(HyperLink)
