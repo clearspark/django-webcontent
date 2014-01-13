57c5424f1f95f088652cf32f531f09848fb78064
Matthys Kroon <matthysk@gmail.com>
2014-01-13 06:59:16 +0200

    Fix bug in admin config from prev commit

--- admin.py
+++ admin.py
@@ -1,11 +1,11 @@
 #Content app
 from django.contrib import admin
-from csdjango.WebContent.models import Page, FileUpload, ContentTag, Hyperlink
+from csdjango.WebContent.models import Page, FileUpload, ContentTag, HyperLink
 
-class FileUploadAdmin(ContentAdmin):
-    list_display = ["slug", "title", "get_static_url"]
+class FileUploadAdmin(admin.ModelAdmin):
+    list_display = ["slug", "get_static_url"]
 
-admin.site.register(Page,PageAdmin)
+admin.site.register(Page)
 admin.site.register(FileUpload, FileUploadAdmin)
 admin.site.register(ContentTag)
 admin.site.register(HyperLink)
