cd206aa6d0c7b27065d5d993367178cb56a64ab7
Matthys <matthysk@clearspark.co.za>
2016-01-02 12:07:02 +0200

    Make app installable.

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+setup(
+    name='django-webcontent',
+    version='0.1',
+    author = "Matthys Kroon",
+    author_email = "matthysk@clearspark.co.za",
+    description='Web content management app for django.',
+    license = "Proprietary, copyright ClearSpark (c) 2012-2015",
+    packages=['WebContent'],
+    include_package_data=True,
+    install_requires=[
+        "django",
+    ]
+)
